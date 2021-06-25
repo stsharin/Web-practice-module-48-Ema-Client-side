f8ed80406f4289049169b444438daab7d28a99fa
Saraban Tasnim Sharin <stssharin@gmail.com>
2021-06-26 01:34:05 +0600

    added the form

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -4,17 +4,25 @@ import fakeData from '../../fakeData';
 const Inventory = () => {
     // sending data or request to add data
     const handleAddProduct = () => {
+        const product = {};
         fetch('http://localhost:5000/addProduct', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(fakeData)
+            body: JSON.stringify(product)
         })
     }
     return (
         <div>
-            <button onClick={handleAddProduct}>Add Products</button>
+            <form action="">
+                <p><span>Name</span><input type="text"></input></p>
+                <p><span>Price</span><input type="text"></input></p>
+                <p><span>Quantity</span><input type="text"></input></p>
+                <p><span>Product Image </span><input type="file"></input></p>
+                <button onClick={handleAddProduct}>Add Products</button>
+            </form>
+
         </div>
     );
 };
