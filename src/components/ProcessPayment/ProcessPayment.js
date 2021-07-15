150e1899688a823cbe2bc36f952bae5d497799e3
Saraban Tasnim Sharin <stssharin@gmail.com>
2021-07-16 02:58:37 +0600

    added pay button

--- src/components/ProcessPayment/ProcessPayment.js
+++ src/components/ProcessPayment/ProcessPayment.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import {CardElement} from '@stripe/react-stripe-js';
+import SimpleCardForm from './SimpleCardForm';
 
 const stripePromise = loadStripe('pk_test_51JDXVOGUvaG0o4MbdIISIO14Vrjx436r6fePxu8jDX19JRkk5EtUx5L0kOhZu6MOdIIjkqQ0ZDyhOT7GjolctPfn00a1jKoiwF');
 
 const ProcessPayment = () => {
     return (
         <Elements stripe={stripePromise}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
-                            },
-                        },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
+            <SimpleCardForm/>
         </Elements>
     );
 };
