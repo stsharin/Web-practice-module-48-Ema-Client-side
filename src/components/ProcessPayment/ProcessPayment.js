37af4648154d5e60787fc599aefbbe45397b349e
Saraban Tasnim Sharin <stssharin@gmail.com>
2021-07-19 02:27:49 +0600

    sliteCardForm added

--- src/components/ProcessPayment/ProcessPayment.js
+++ src/components/ProcessPayment/ProcessPayment.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import SimpleCardForm from './SimpleCardForm';
+import SplitCardForm from './SliteCardForm';
 
 const stripePromise = loadStripe('pk_test_51JDXVOGUvaG0o4MbdIISIO14Vrjx436r6fePxu8jDX19JRkk5EtUx5L0kOhZu6MOdIIjkqQ0ZDyhOT7GjolctPfn00a1jKoiwF');
 
 const ProcessPayment = () => {
     return (
         <Elements stripe={stripePromise}>
-            <SimpleCardForm/>
+            <SplitCardForm/>
         </Elements>
     );
 };
